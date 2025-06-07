d3e720b87f31b528a59e2b7cb47e7c5251107bbd
Will Morris <morris.william.work@gmail.com>
2025-06-07 10:34:36 -0400

    Add configurable reduction percent

--- src/main/net/williserver/firereduce/FireReducePlugin.kt
+++ src/main/net/williserver/firereduce/FireReducePlugin.kt
@@ -11,7 +11,11 @@ class FireReducePlugin: JavaPlugin() {
     private val logHandler = LogHandler(logger)
 
     override fun onEnable() {
-        server.pluginManager.registerEvents(FireSpreadListener(), this)
+        saveDefaultConfig()
+        val percentCancelled = loadConfigPercentage()
+        logHandler.info("Loaded cancellation percentage: $percentCancelled")
+
+        server.pluginManager.registerEvents(FireSpreadListener(percentCancelled), this)
         logHandler.info("Server event listeners registered.")
 
         logHandler.info("FireReduce enabled!")
@@ -21,7 +25,26 @@ class FireReducePlugin: JavaPlugin() {
         logHandler.info("FireReduce disabled!")
     }
 
+    /**
+     * Acceptable range of fire cancellation: 0-100%.
+     * @return the loaded percent if within the acceptable range, otherwise DEFAULT_CANCEL_PERCENT (50)
+     */
+    private fun loadConfigPercentage() =
+        config.getInt("percentCancelled").let {
+            if (it in 0..100) {
+                it
+            } else {
+                logHandler.err("Fire cancellation percent must be between 0 and 100, inclusive. Loaded: $it. Using percent $DEFAULT_CANCEL_PERCENT.")
+                DEFAULT_CANCEL_PERCENT
+            }
+        }
+
     companion object {
         const val PLUGIN_PREFIX = "[FireReduce]"
+
+        /**
+         * Default percent of fires to cancel, if option not loaded.
+         */
+        const val DEFAULT_CANCEL_PERCENT = 50
     }
 }
--- src/main/net/williserver/firereduce/FireSpreadListener.kt
+++ src/main/net/williserver/firereduce/FireSpreadListener.kt
@@ -7,14 +7,22 @@ import org.bukkit.event.block.BlockSpreadEvent
 
 /**
  * Listen for fire spread events and cancel them proportionally to the provided cancellation percent.
+ * @param cancelPercent Percent of fire spreads to cancel.
  *
  * @author Willmo3
  */
-class FireSpreadListener: Listener {
+class FireSpreadListener(private val cancelPercent: Int): Listener {
+    init {
+        if (cancelPercent !in (0..100)) {
+            throw IllegalArgumentException("cancelPercent must be in range 0..100")
+        }
+    }
+
     @EventHandler
     fun onFireSpread(event: BlockSpreadEvent) {
         if (event.source.type == Material.FIRE) {
-            if ((1..100).random() <= 90) {
+            // Note: never true for percent = 0, by design. Allows turning off cancellation entirely.
+            if ((1..100).random() <= cancelPercent) {
                 event.isCancelled = true
             }
         }
